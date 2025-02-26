fa4d90824416f06d74a5b5997a80863b6e4f7934
GrinevaNataly <162015539+NatalyGit@users.noreply.github.com>
2025-02-26 16:46:46 +0300

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -1,8 +1,7 @@
 from datetime import datetime, timedelta
 def future_date(days_from_now):
 """
-Возвращает дату, которая наступит через указанное количество
-дней от текущей даты.
+Возвращает дату, которая наступит через указанное количество дней от текущей даты.
 :param days_from_now: Количество дней от текущей даты.
 :return: Отформатированная дата в формате YYYY-MM-DD.
 Примеры:
@@ -18,6 +17,5 @@ future_date = today + timedelta(days=days_from_now)
 
 formatted_future_date = future_date.strftime('%Y-%m-%d')
 return formatted_future_date
-if __name__ == '__main__':
-days = 30 # Количество дней для вычисления
+if __name__ == '__main__': days = 30 
 print(f'Date {days} days from now: {future_date(days)}')
